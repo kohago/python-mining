24704e15b14c3a4439a4cefdb8bb3842b74e8cab
kouha.shu <k-syuu@msn.com>
2018-05-22 17:20:47 +0900

    read big json use stream

--- json/testJson.py
+++ json/testJson.py
@@ -4,7 +4,7 @@ import io
 import json
 import simplejson
 import ijson
-from ijson import parse
+from ijson import parse,ObjectBuilder
 import time
 
 # S3バケット名
@@ -27,6 +27,7 @@ def do_one():
     unzipFile=GzipFile(None, 'rb', fileobj=buffer)
     #for prefix, event, value in parser:
     #    print('prefix={}, event={}, value={}'.format(prefix, event, value))
+    #### this is not stream!
     ones = ijson.items(unzipFile,'ones.item')
     #for one in ones:
     #  print(json.dumps(one))
@@ -39,25 +40,41 @@ def do_two():
     f = open(KEY2,'rb')
     buffer = io.BytesIO(f.read())
     unzipFile=GzipFile(None, 'rb', fileobj=buffer)
+    #### this is not stream!
     twos = ijson.items(unzipFile,'twos')
     with open('twos_big.json','w') as outFile:
         for two in twos:
             outFile.write("\n".join(simplejson.dumps(twoValue) for twoValue in two.values()))
     #s3_client.upload_fileobj(GzipFile(None, 'rb', fileobj=buffer), S3_BUCKET_TO, "twos_loop_2000.json")
 
+def big_file_stream(unzipFile):
+    print('analysing big json file using streaming  ...')
+    key='-'
+    for prefix, event, value in parse(unzipFile):
+        if prefix =='somePrefix' and  event == 'map_key':
+            key = value
+            builder = ObjectBuilder()
+        elif prefix.startswith('somePrefix.' + str(key)):
+            builder.event(event,value)
+            if event == 'end_map':
+                yield key,builder.value
+
 def do_big_two():
     start = time.time()
-    f = open(KEY3,'rb')
+    print('begin!')
+    f = open(KEY4,'rb')
     buffer = io.BytesIO(f.read())
     unzipFile=GzipFile(None, 'rb', fileobj=buffer)
-    twos = ijson.items(unzipFile,'twos')
-    with open('twos.json','w') as outFile:
-        for two in twos:
-            outFile.write("\n".join(simplejson.dumps(twoValue) for twoValue in two.values()))
+    print('ready to read  zip file! ')
+
+    with open('two_big.json','w') as outFile:
+        print('begin to read zip file by streaming')
+        for key,value in big_file_stream(unzipFile):
+           outFile.write("\n".join(simplejson.dumps(value)))
+
     elapsed_time = time.time() - start
     print ("elapsed_time:{0}".format(elapsed_time) + "[sec]")
 
 #do_one()
 #do_two()
 do_big_two()
-
